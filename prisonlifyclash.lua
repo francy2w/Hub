21835fe43f55d6c8652cb2a0ba507a7dae3196d7
francy2w <francyqpp@gmail.com>
2026-07-29 00:00:33 +0200

    Delete prisonlifyclash.lua

--- prisonlifyclash.lua
+++
@@ -1,7 +0,0 @@
-game:GetService("ReplicatedStorage"):FindFirstChild("E&F").Offline.GetOfflineCoins:FireServer(9e300)
-
-game:GetService("ReplicatedStorage")["E&F"].Eco.AddEcoRE:FireServer(table.unpack({
-    [1] = "coin",
-    [2] = 9e300,
-    [3] = "Stage",
-}))
